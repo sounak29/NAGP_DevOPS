7085af586c218f20352bbd0c7f94738ceb22555f
sounak29 <sounak.roy@tungsten-network.com>
2023-08-07 22:09:08 +0530

    checking again

--- src/main/java/com/magento/utill/FileWrite.java
+++ src/main/java/com/magento/utill/FileWrite.java
@@ -8,19 +8,22 @@ import org.apache.logging.log4j.Logger;
 public class FileWrite {
 	static Logger log = LogManager.getLogger(FileWrite.class);
 
-	public static void writeFile(String value, String path)
+	public static void writeFile(String value, String path) throws IOException
 	{
 		log.info("Write File Method called");
-
+		FileWriter writer=null;
 		try
 		{
-			FileWriter writer=new FileWriter(path);
+			 writer=new FileWriter(path);
 			writer.write(String.valueOf(value));
-			writer.close();
 		}
 		catch(IOException e)
 		{
 			e.printStackTrace();
+		}
+		finally
+		{
+			writer.close();
 		}
 	}
 }
